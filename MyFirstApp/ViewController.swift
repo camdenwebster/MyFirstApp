0452e993e227277f200077070de15982fdd1ec15
Camden Webster <cwebster@students.mchenry.edu>
2024-02-06 22:04:35 -0600

    Add exercises for Chapter 4

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,17 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: Properties
     @IBOutlet var labelResult: UILabel!
-
-    @IBOutlet weak var textMessage: UITextField!
-    @IBAction func changeButton(_ sender: UIButton) {
-        labelResult.text = textMessage.text?.uppercased()
-    }
+    @IBOutlet weak var textDisplay: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Methods
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textDisplay.text?.uppercased()
+        print (labelResult.text!)
+    }
 
 }
 
